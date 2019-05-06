0f9386b0ba547c6df5abc6b371694b3cbe1e1fc7
Balthild <ibalthild@gmail.com>
2019-05-06 23:16:34 +0800

    Bookmarks page

---
+++ scripts/bookmarks.js
@@ -0,0 +1,77 @@
+const { safeLoad } = require('js-yaml');
+const { parse, stringify } = require('css');
+
+const css = stringify(parse(`
+.bookmarks {
+    list-style: none;
+    padding: 0;
+    margin: 1em 0;
+    display: flex;
+    flex-flow: row wrap;
+}
+
+.bookmarks li {
+    display: block;
+    width: 50%;
+}
+
+.bookmarks li a {
+    color: rgba(0, 0, 0, 0.7) !important;
+    display: block;
+    min-height: 64px;
+    position: relative;
+    box-sizing: content-box;
+    padding: 12px 12px 12px 96px;
+    border-radius: 2px;
+}
+
+.bookmarks li a:hover {
+    background: rgba(0, 0, 0, 0.03);
+}
+
+.bookmarks li a img {
+    width: 64px;
+    height: 64px;
+    border-radius: 32px;
+    position: absolute;
+    top: 12px;
+    left: 12px;
+}
+
+.bookmarks li a h4 {
+    margin: 8px 0 12px;
+}
+
+.bookmarks li a p {
+    word-wrap: none;
+    word-break: keep-all;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+}
+
+@media screen and (max-width: 600px) {
+    .bookmarks li {
+        width: 100%;
+    }
+}
+`), { compress: true });
+
+hexo.extend.tag.register('bookmarks', function (args, content) {
+    const doc = safeLoad(content);
+
+    const items = doc.map(({ name, description, url, avatar }) => `
+        <li>
+            <a href="${url}" title="${name}" target="_blank">
+                <img src="${avatar}" />
+                <h4>${name}</h4>
+                <p>${description}</p>
+            </a>
+        </li>
+    `.trim()).join('');
+
+    return `
+        <style>${css}</style>
+        <ul class="bookmarks">${items}</ul>
+    `;
+}, { ends: true });
---
+++ source/links/index.md
@@ -0,0 +1,42 @@
+---
+title: Friends
+---
+
+总之这里面全是 dalao 啦（
+
+{% bookmarks %}
+- name: Alien
+  description: 是网络工程 dalao
+  url: https://alien.bz/
+  avatar: https://alien.bz/content/images/2018/08/avatar.jpg
+
+- name: Andy1999
+  description: 是运维 dalao
+  url: https://kotori.net/
+  avatar: https://cn.gravatar.com/avatar/9d7ad341cd823c5d2f8c7bab440551f2?s=128
+
+- name: Awoer
+  description: 是超单纯的 Aower (
+  url: http://blog.awoer.com/
+  avatar: https://cn.gravatar.com/avatar/?s=128&d=mm
+
+- name: DIYgod
+  description: 是前端 dalao
+  url: https://diygod.me
+  avatar: https://diygod.me/images/DIYgod.jpg
+
+- name: Kenvix
+  description: 是云签到作者 dalao
+  url: https://kenvix.com/
+  avatar: https://cn.gravatar.com/avatar/11804ec6b3178d7f4ada6177d224de2d?s=128
+
+- name: NetherCrow CSOLYOO
+  description: 是乌鸦 dalao
+  url: https://www.utsuho.win/
+  avatar: https://cn.gravatar.com/avatar/06c1e9d8debdca6a295868ebd634411d?s=128
+
+- name: 吟梦酱
+  description: 是前端 & ACM dalao
+  url: https://kirainmoe.com/
+  avatar: https://cn.gravatar.com/avatar/3d19f6408d50f41e066b23f05bb9c6ad?s=128
+{% endbookmarks %}
